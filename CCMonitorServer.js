8910f17c2db9ce3c44f4cc30bb2d5e06f388d06e
= <me@zombiejerky.net>
2017-01-10 14:59:58 -0500

    fixed redirect for legecy iframe

--- CCMonitorServer.js
+++ CCMonitorServer.js
@@ -4,6 +4,7 @@ var listener = new require("./listener.js")(25580);
 var express = require('express')
 var app = express()
 
+app.use("/netizen539/Servers" ,express.static('static'))
 app.use(express.static('static'))
 app.get('/apiPre', function (req, res) {
 	console.log("serving /apiPre", Date.now());
